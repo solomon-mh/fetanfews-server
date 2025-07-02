7b74686ad67d9d21c90521c162688a3ee80234dc
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-02 11:45:23 +0300

    Add MedicationController with CRUD methods and search functionality

---
+++ app/Http/Controllers/MedicationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medication;
+use Illuminate\Http\Request;
+
+
+class MedicationController extends Controller
+{
+   
+    public function index()
+    {
+        //
+    }
+
+   
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show(Medication $medication)
+    {
+        //
+    }
+
+ 
+    public function edit(Medication $medication)
+    {
+        //
+    }
+
+   
+    public function update(Request $request, Medication $medication)
+    {
+        //
+    }
+
+
+    public function destroy(Medication $medication)
+    {
+        //
+    }
+    public function search(Request $request){
+        $search = $request->query('medication');
+        $medicaitons = Medication::query()->when($search, function($query,$search){
+            $query->where('name', 'like', '%'. $search . '%');
+        })->get();
+        return response()->json($medicaitons);
+    }
+}
