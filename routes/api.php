361edfb57d66b67b557d6d7fd948072a6f6a12e7
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-02 11:47:13 +0300

    Add API routes for user authentication and pharmacy/medication management

---
+++ routes/api.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\PharmacyController;
+use App\Http\Controllers\MedicationController;
+
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+    // Pharmacies API
+    
+    Route::prefix('pharmacies')->group(function () {
+        Route::get('/', [PharmacyController::class, 'index']);
+        
+        Route::get('/browse_by_categories', [PharmacyController::class, function(){
+            return [];
+        }]);
+        
+        Route::get('/most-searched-medications', [PharmacyController::class, function(){
+            return ['Good response'];
+        }]); 
+        Route::get('/search', [PharmacyController::class, 'search']); 
+        Route::get('/{pharmacy}', [PharmacyController::class, 'show']);
+});
+    // Medications API
+   Route::prefix('medications')->group(function() {
+    Route::get('/search', [MedicationController::class,'search']);
+   });
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
-use App\Http\Controllers\PharmacyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome';
+    return ['Laravel' => app()->version()];
 });
-Route::get('/hello',function(){
-    return '<h1>Hello There!</h1>';
-});
-// Pharmacies api
-Route::prefix('api')->group(function(){
-    Route::get('/pharmacies', [PharmacyController::class,'index']);
-    Route::get('/browse_by_categories',function(){
-        return [];
-    });
-    Route::get('/most-searched-medications',function(){
-        return [];
-    });
-});
+
+require __DIR__.'/auth.php';
