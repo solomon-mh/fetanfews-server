33e20afffd6b172e14615d448f7a403f159b9fa8
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-10 16:37:21 +0300

    Add phone and role fields to users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['user', 'admin', 'pharmacist']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
