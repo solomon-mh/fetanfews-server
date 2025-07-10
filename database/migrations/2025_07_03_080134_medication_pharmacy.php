72aa41fba84d629723f1becec4fdb6ddb27191a1
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-10 16:37:33 +0300

    Refactor medication_pharmacy migration: rename stock_quantity to quantity_available for clarity

--- database/migrations/2025_07_03_080134_medication_pharmacy.php
+++ database/migrations/2025_07_03_080134_medication_pharmacy.php
@@ -11,18 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-    Schema::create('medication_pharmacy', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('medication_id')->constrained()->onDelete('cascade');
-        $table->foreignId('pharmacy_id')->constrained()->onDelete('cascade');
-        $table->decimal('price', 8, 2)->nullable();         // e.g., 5.00
-        $table->integer('stock_quantity')->default(0);      // e.g., 100 units
-        $table->boolean('stock_status')->default(true);
-        $table->integer('quantity_available');
-        $table->string('manufacturer');
-        $table->timestamps();
-    });
-
+        Schema::create('medication_pharmacy', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medication_id')->constrained()->onDelete('cascade');
+            $table->foreignId('pharmacy_id')->constrained()->onDelete('cascade');
+            $table->decimal('price', 8, 2)->nullable();         // e.g., 5.00
+            $table->boolean('stock_status')->default(true);
+            $table->integer('quantity_available');
+            $table->string('manufacturer');
+            $table->timestamps();
+        });
     }
 
     /**
