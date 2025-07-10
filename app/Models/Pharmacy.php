4aed47fcc8e25bfb4d803746ae8679507dba583c
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-10 16:36:58 +0300

    Refactor Pharmacy model: update fillable properties and adjust medications relationship pivot fields

--- app/Models/Pharmacy.php
+++ app/Models/Pharmacy.php
@@ -5,15 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Medication;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pharmacy extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name','email','address','phone','website','operating_hours','latitude','longitude','image','status','is_verified','delivery_available'
+        'name',
+        'email',
+        'address',
+        'phone',
+        'website',
+        'operating_hours',
+        'latitude',
+        'longitude',
+        'image',
+        'status',
+        'is_verified',
+        'delivery_available',
+        'license_number',
+        'license_image',
+        'user_id'
+
+    ];
+    protected $casts = [
+        'delivery_available' => 'boolean'
     ];
 
-    public function medications(){
-        return $this->belongsToMany(Medication::class)->withPivot(['price','stock_quantity','stock_status','quantity_available','manufacturer'])->withTimeStamps();
+    public function medications()
+    {
+        return $this->belongsToMany(Medication::class)->withPivot(['price', 'quantity_available', 'stock_status', 'manufacturer'])->withTimeStamps();
+    }
+    public function users()
+    {
+        return $this->belongsTo(User::class)->withTimeStamps();
     }
 };
