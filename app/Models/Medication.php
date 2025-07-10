1d73e48806df778d5f9a6e601db8b09c057c881e
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-10 16:36:45 +0300

    Remove 'stock_quantity' from Medication pharmacies pivot fields

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -10,7 +10,13 @@ class Medication extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    public function pharmacies(){
-        return $this->belongsToMany(Pharmacy::class)->withPivot(['price','stock_quantity','stock_status','quantity_available','manufacturer'])->withTimeStamps();
+    protected $casts = ['stock_status' => 'boolean', 'prescription_required' => 'boolean'];
+    public function pharmacies()
+    {
+        return $this->belongsToMany(Pharmacy::class)->withPivot(['price', 'quantity_available', 'stock_status', 'manufacturer'])->withTimeStamps();
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
